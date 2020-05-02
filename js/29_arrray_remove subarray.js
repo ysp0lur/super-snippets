21cdd935d236809ed68ec6bd79bce6cc7c2ed9f8
Giulia Chiola <giulia.chiola.17@gmail.com>
2020-05-02 22:14:24 +0200

    feat: add JS ok only

--- js/29_arrray_remove subarray.js
+++
@@ -1,8 +0,0 @@
-/*
-  ======================================== Arrays: remove all elements contained in another array ====================================================
-*/
-
-let elements = [1,2,3,4,5,6,7,8]
-const toRemove = [1,2,5]
-
-elements = elements.filter(el => !toRemove.includes(el)) // [3, 4, 6, 7, 8]
